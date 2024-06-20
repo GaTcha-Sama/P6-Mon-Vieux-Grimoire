a1e4b09623d932aa64e0d4e84d0cddcb75724e39
Jeremy Louet <jeremy.louet29@hotmail.fr>
2024-06-20 12:08:22 +0200

    Version def avec opti des images

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,4 +1,7 @@
 const multer = require('multer');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -8,15 +11,32 @@ const MIME_TYPES = {
   'image/webp': 'webp'
 };
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images');
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
-  }
-});
+const storage = multer.memoryStorage();
 
-module.exports = multer({storage: storage}).single('image');
+const upload = multer({ storage: storage }).single('image');
+
+module.exports = (req, res, next) => {
+  upload(req, res, async (err) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    if (req.file) {
+      const outputPath = path.join('images', req.file.originalname.split(' ').join('_').split('.')[0] + Date.now() + '.webp');
+      
+      try {
+        await sharp(req.file.buffer)
+          .webp({ quality: 80 }) // Optimisation de la qualité de l'image WebP //
+          .toFile(outputPath);
+
+        // Mise à jour du chemin du fichier et du nom du fichier pour pointer vers le fichier WebP //
+        req.file.path = outputPath;
+        req.file.filename = path.basename(outputPath);
+        next();
+      } catch (err) {
+        return res.status(500).json({ error: err.message });
+      }
+    } else {
+      next();
+    }
+  });
+};
